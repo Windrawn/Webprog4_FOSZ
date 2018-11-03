2a3e91ca326f4cb93648b222f0931bee8c35d74d
Windrawn <43348627+Windrawn@users.noreply.github.com>
2018-11-03 11:22:38 +0100

    cica v1.0.1

--- Projekt/app.js
+++ Projekt/app.js
@@ -6,6 +6,8 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.use('/', weboldalRoutes);
 
 
--- Projekt/routes/weboldal.js
+++ Projekt/routes/weboldal.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/', function (req, res) {
-	res.render('pages/home');
+	res.render('pages/login');
 });
 
 
