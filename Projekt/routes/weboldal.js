265fdd418ba08b749e5246d5572264f375a615fd
Windrawn <43348627+Windrawn@users.noreply.github.com>
2018-11-04 15:21:17 +0100

    Routes-ok hozzáadása + EJS-ek létrehozása.

--- Projekt/routes/weboldal.js
+++ Projekt/routes/weboldal.js
@@ -10,4 +10,21 @@ router.get('/index', function (req, res) {
 	res.render('pages/index');
 });
 
+router.get('/fajtak', function (req, res) {
+	res.render('pages/fajtak');
+});
+
+router.get('/betegsegek', function (req, res) {
+	res.render('pages/betegsegek');
+});
+
+router.get('/erdekessegek', function (req, res) {
+	res.render('pages/erdekessegek');
+});
+
+router.get('/bejegyzes', function (req, res) {
+	res.render('pages/bejegyzes');
+});
+
+
 module.exports = router;
