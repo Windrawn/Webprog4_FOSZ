0b9072b47d855a33944e34c03d9260834a1fe0e4
Kocsis Dániel <43856778+KeyDaniel98@users.noreply.github.com>
2018-11-04 16:17:15 +0100

    ejs készítés/update
    login.ejs véglegesítése
    home.ejs alapjainak elkezdése
    maradék hiányzó ejs-ek feltöltése

--- Projekt/routes/weboldal.js
+++ Projekt/routes/weboldal.js
@@ -3,6 +3,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
+	res.render('pages/home');
+});
+
+router.get('/login', function (req, res) {
 	res.render('pages/login');
 });
 
