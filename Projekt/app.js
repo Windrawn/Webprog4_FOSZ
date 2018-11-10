a3875760690b265836c7349b4afb25f275fcc7a8
tremlferenc <02feri02@gmail.com>
2018-11-11 00:52:44 +0100

    bejegyzesek route hozzaadva

--- Projekt/app.js
+++ Projekt/app.js
@@ -3,11 +3,11 @@ var express = require('express');
 var weboldalRoutes = require('./routes/weboldal');
 var teszterRoutes = require('./routes/teszter');
 var userRoutes = require('./routes/user');
+var bejegyzesekRoutes = require('./routes/bejegyzesek');
 
 var app = express();
 
 var session = require('express-session');
- 
 app.use(session({
   secret: 'nagyontitkoskiscuccnehogyatirdmerteljovokerted',
   resave: false,
@@ -27,6 +27,6 @@ app.use(bodyParser.json());
 app.use('/', weboldalRoutes);
 app.use('/', teszterRoutes);
 app.use('/', userRoutes);
-
+app.use('/', bejegyzesekRoutes);
 
 app.listen(8080); 
