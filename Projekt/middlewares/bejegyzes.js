c27e80dfec4ececeda2b6b1712f2cbc992a3d897
Kocsis Dániel <43856778+KeyDaniel98@users.noreply.github.com>
2018-11-11 14:55:55 +0100

    dinamikus navbár, oldalak dinamikus felépítése adatbázisból
    home.ejs, fajtak.ejs

---
+++ Projekt/middlewares/bejegyzes.js
@@ -0,0 +1,29 @@
+function login(objectRepository) {
+	return function(req, res, next) {
+
+		hashedUser = {
+			username: req.body.username,
+			password: objectRepository.sha512.sha512(req.body.password).toUpperCase()
+		}
+
+		objectRepository.modelUser.find(hashedUser, function(next) {
+			console.log('Bejelentkezési kérelem'); return next;
+		}).then(function (response) {
+			if (Array.isArray(response) && response.length) {
+				console.log('Sikeres Bejelentkezés');
+				req.session.user = user;
+				req.session.login = true;
+				return res.redirect('/');
+				
+				
+			}
+			else {
+				console.log('Sikertelen Bejelentkezés!');
+				return res.redirect('/login');
+				
+			}
+		})
+	}
+}
+
+module.exports = login;
