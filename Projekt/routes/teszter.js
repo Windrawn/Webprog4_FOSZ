48cbb28d1904a22eb7353035947d744d29342b9b
Kocsis Dániel <43856778+KeyDaniel98@users.noreply.github.com>
2018-11-13 17:01:03 +0100

    erdekessegek.ejs végsőkinézet
    működik meg minden, szerkesztés gomb még nem megy!

--- Projekt/routes/teszter.js
+++ Projekt/routes/teszter.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/teszt', function (req, res) {
-	res.render('pages/temak/betegsegek',{bejegyzesek: []});
+	res.render('pages/temak/erdekessegek',{bejegyzesek: []});
 });
 
 module.exports = router;
