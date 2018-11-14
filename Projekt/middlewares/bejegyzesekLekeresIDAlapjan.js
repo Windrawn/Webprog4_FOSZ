b11a498e9e8f12ae0580bf2ac93f6933a846c0ed
tremlferenc <02feri02@gmail.com>
2018-11-14 22:31:59 +0100

    bejegyzesek lekeres ID alapjan implementalva

---
+++ Projekt/middlewares/bejegyzesekLekeresIDAlapjan.js
@@ -0,0 +1,17 @@
+var a;
+
+function bejegyzesekLekeresIDAlapjan(objectRepository, id, callback) {
+		objectRepository.Bejegyzesekmodel.find({id: id})
+		.exec(function (err, response) {
+			if (Array.isArray(response) && response.length) {
+				a = response;
+				callback(a);
+			}
+			else {
+				console.log("Hiba a bejegyzes lekeresben");
+			}
+		})
+		return null;
+}
+
+module.exports = bejegyzesekLekeresIDAlapjan;
