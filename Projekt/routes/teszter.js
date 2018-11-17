a2af5866fa5b9d05de30be8cc81e91e852e03a13
Kocsis Dániel <43856778+KeyDaniel98@users.noreply.github.com>
2018-11-17 17:56:50 +0100

    Admin felület + javítás
    Admin kezelőfelület elkezdése
    tema.ejs javítása - nem voltak linkek

--- Projekt/routes/teszter.js
+++ Projekt/routes/teszter.js
@@ -3,13 +3,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/teszt', function (req, res) {
-	res.render('pages/bejegyzes',{bejegyzes: {
-		username: '1',
-		tema: '1',
-		cim: '1',
-		tartalom: '1',
-		kep: '1'
-	}});
+	res.render('pages/admin/users',{users: [
+		{
+		_id: 1,
+		username: "Test1",
+		password: "",
+		email: "test1@test.hu",
+		admin: true
+		},
+		{
+		_id: 2,
+		username: "Test2",
+		password: "",
+		email: "test2@test.hu",
+		admin: false
+		}
+	]});
+});
+
+router.get('/teszt2', function (req, res) {
+	res.render('pages/admin/user', {mod: "adminNew"});
 });
 
 module.exports = router;
